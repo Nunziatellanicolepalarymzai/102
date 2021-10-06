8028590a4ce0ed55e3cec77d760d4879b26156de
Nunziatellanicolepalarymzai <74307514+Nunziatellanicolepalarymzai@users.noreply.github.com>
2021-10-06 14:56:22 +0530

    Add files via upload

---
+++ captureimage.py
@@ -0,0 +1,20 @@
+import cv2
+
+def take_snapshot():
+    # initializing cv2
+    videocaptureobject = cv2.VideoCapture(0)
+    result=True
+    while(result):
+        #read the frames while the camera is on.
+        ret,frame=videocaptureobject.read()
+        print(ret)
+
+        cv2.imwrite("xyz.jpg",frame)
+        #cv2.imwrite():-meathod is used to store an image into given path
+        result=False
+
+    videocaptureobject.release() 
+    #release the camera
+    cv2.destroyAllWindows()
+
+take_snapshot()
